d9175aee68ea84826ca8881ae06978987bff883c
Baek Gi Chan <laphayen@gmail.com>
2024-06-10 23:56:57 +0900

    #19 - Test registration of Pharmacy entity inheriting BaseTimeEntity
    Test registration of Pharmacy entity inheriting BaseTimeEntity.

--- src/test/groovy/com/laphayen/pharmacyrecommendation/api/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/laphayen/pharmacyrecommendation/api/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -4,6 +4,8 @@ import com.laphayen.pharmacyrecommendation.AbstractIntegrationContainerBaseTest
 import com.laphayen.pharmacyrecommendation.api.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
 
+import java.time.LocalDateTime
+
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     @Autowired
@@ -59,6 +61,26 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         then:
         result.size() == 1
 
+    }
+
+    def "BaseTimeEntity 등록"() {
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getCreatedDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
     }
 
 }
