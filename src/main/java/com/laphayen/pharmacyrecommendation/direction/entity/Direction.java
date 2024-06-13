4bd478b2de7dff1896e22f240c8308652a3cb5ce
Baek Gi Chan <laphayen@gmail.com>
2024-06-13 23:54:35 +0900

    #25 - Add entity class for storing distance
    Store distance information based on the customer's location and the pharmacy's location in the pharmacy recommendation system.

---
+++ src/main/java/com/laphayen/pharmacyrecommendation/direction/entity/Direction.java
@@ -0,0 +1,37 @@
+package com.laphayen.pharmacyrecommendation.direction.entity;
+
+import com.laphayen.pharmacyrecommendation.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "direction")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Direction extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 고객
+    private String inputAddress;
+    private double inputLatitude;
+    private double inputLongitude;
+
+    // 약국
+    private String targetPharmacyName;
+    private String targetAddress;
+    private double targetLatitude;
+    private double targetLongitude;
+
+    // 고객 주소 와 약국 주소 사이의 거리
+    private double distance;
+
+}
