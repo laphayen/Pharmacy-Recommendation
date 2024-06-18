721c1ec3f62f62f6264f392bba950b954466df37
Baek Gi Chan <laphayen@gmail.com>
2024-06-18 23:43:03 +0900

    #37 - Modify to return a list of pharmacy recommendations.
    Modify the recommendPharmacyList method to return a list of pharmacy recommendations based on the given address.

--- src/main/java/com/laphayen/pharmacyrecommendation/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/laphayen/pharmacyrecommendation/pharmacy/service/PharmacyRecommendationService.java
@@ -3,6 +3,7 @@ package com.laphayen.pharmacyrecommendation.pharmacy.service;
 import com.laphayen.pharmacyrecommendation.api.dto.DocumentDto;
 import com.laphayen.pharmacyrecommendation.api.dto.KakaoApiResponseDto;
 import com.laphayen.pharmacyrecommendation.api.service.KakaoAddressSearchService;
+import com.laphayen.pharmacyrecommendation.direction.dto.OutputDto;
 import com.laphayen.pharmacyrecommendation.direction.entity.Direction;
 import com.laphayen.pharmacyrecommendation.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,13 +24,13 @@ public class PharmacyRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
-    public void recommendPharmacyList(String address) {
+    public List<OutputDto> recommendPharmacyList(String address) {
 
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
         if(Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
             log.error("[PharmacyRecommendationService recommendPharmacyList fail] Input address: {}", address);
-            return;
+            return Collections.emptyList();
         }
 
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
@@ -38,8 +41,20 @@ public class PharmacyRecommendationService {
         // kakao 카테고리를 이용한 장소 검색 api 이용
         // List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
 
-        directionService.saveAll(directionList);
+        return directionService.saveAll(directionList)
+                .stream()
+                .map(this::convertToOutputDto)
+                .collect(Collectors.toList());
+    }
 
+    private OutputDto convertToOutputDto(Direction direction) {
+        return OutputDto.builder()
+                .pharmacyName(direction.getTargetPharmacyName())
+                .pharmacyAddress(direction.getTargetAddress())
+                .directionUrl("todo")   // TODO
+                .roadViewUrl("todo")
+                .distance(String.format("%.2f km", direction.getDistance()))
+                .build();
     }
     
 }
