dea394142637e60d06cf57d922404356d5e1b0c3
Baek Gi Chan <laphayen@gmail.com>
2024-06-18 23:37:46 +0900

    #37 - Add OutputDto
    Add OutputDto.

---
+++ src/main/java/com/laphayen/pharmacyrecommendation/direction/dto/OutputDto.java
@@ -0,0 +1,16 @@
+package com.laphayen.pharmacyrecommendation.direction.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class OutputDto {
+
+    private String pharmacyName;
+    private String pharmacyAddress;
+    private String directionUrl;
+    private String roadViewUrl;
+    private String distance;
+
+}
