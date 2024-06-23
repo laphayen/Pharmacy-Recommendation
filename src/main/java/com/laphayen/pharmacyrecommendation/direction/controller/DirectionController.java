73cdbef99c7ffce39d591ef4ec6f08a914ee4c78
Baek Gi Chan <laphayen@gmail.com>
2024-06-24 04:54:13 +0900

    #49 - Modify the test using dependency injection
    Modify the test using dependency injection.

--- src/main/java/com/laphayen/pharmacyrecommendation/direction/controller/DirectionController.java
+++ src/main/java/com/laphayen/pharmacyrecommendation/direction/controller/DirectionController.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 @Slf4j
@@ -16,19 +15,12 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class DirectionController {
 
     private final DirectionService directionService;
-    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
 
     @GetMapping("/dir/{encodedId}")
     public String searchDirection(@PathVariable("encodedId") String encodedId) {
-        Direction resultDirection = directionService.findById(encodedId);
+        String result = directionService.findDirectionUrlById(encodedId);
 
-        String params = String.join(",", resultDirection.getTargetPharmacyName(),
-                String.valueOf(resultDirection.getTargetLatitude()), String.valueOf(resultDirection.getTargetLongitude()));
-
-        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params)
-                .toUriString();
-        log.info("direction params: {}, url: {}" ,params ,result);
-
+        log.info("[DirectionController searchDirection] direction url: {}", result);
         return "redirect:"+result;
     }
 }
